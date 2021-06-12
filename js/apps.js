d249a51c48bb365e690de7b454d4a7747255d980
Adnan20201 <afifuladnan2020@gmail.com>
2021-06-12 19:39:45 +0600

    Add function

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,13 @@
 $(function (){
-    
+    $('#slider-container').slick({
+        prevArrow:'<i class="slide-arrow slide-arrow-left fas fa-caret-left"></i>',
+        nextArrow:'<i class="slide-arrow slide-arrow-right fas fa-caret-right"></i>',
+        autoplay:true,
+        autoplaySpeed: 3000,
+        dots:true,
+        dotsClass: 'slide_dots',
+    });
+    $('#counter').counterUp({
+        time:3000,
+    })
 })
